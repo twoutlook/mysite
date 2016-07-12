1c56f3cca303ee47d699fa3c5d56ce54121d0a3a
Mark <twcloudwebapp@outlook.com>
2016-07-12 14:13:25 +0800

    end of part 2

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+import datetime
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 
@@ -9,7 +10,6 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
-
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 @python_2_unicode_compatible  # only if you need to support Python 2
